2e6110b76fd22b333401c377761a4389a582ac9e
JasmienClaeys <jasmien.claeys@student.ehb.be>
2021-11-23 18:49:33 +0100

    S6-express

---
+++ S6/express/index.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.use(express.static('public'));
+
+
+app.get('/', (req, res) => {
+    console.log('local root called!');
+    res.redirect('/info.html');
+});
+
+// app.get('/', (req, res) => {
+//     console.log('local root called!');
+//     res.send('Hello Jasmien')
+// });
+
+//port + function
+app.get('/test', (req, res) => {
+    res.send('test succeeded!');
+});
+
+app.get('/data', (req, res) => {
+    let exampleData = {
+        name: 'Jasmien',
+        age: 20
+    }
+    res.send(exampleData);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
